445ed95cedab0eac9d436328ba8418e34dc6f225
sen329 <hans.hilman@gmail.com>
2025-10-19 13:39:30 +0700

    Streamlined DB connection

--- database/db.go
+++ database/db.go
@@ -9,7 +9,10 @@ import (
 
 func Database() error {
 	var err error
-	DB, err = gorm.Open(mysql.Open(DBUrl(BuildConfig())), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(DBUrl(BuildConfig())), &gorm.Config{
+		SkipDefaultTransaction: true, // Improves performance by avoiding auto-transactions.
+		PrepareStmt:            true, // Caches compiled statements for performance and helps prevent SQL injection.
+	})
 	if err != nil {
 		return err
 	}
--- main.go
+++ main.go
@@ -7,9 +7,6 @@ import (
 	"sports-backend-api/migrations"
 	"sports-backend-api/routes"
 	"sports-backend-api/util"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
@@ -17,15 +14,7 @@ func main() {
 	util.LoadEnv()
 
 	// Initialize database connection
-	dbConfig := database.BuildConfig()
-	dsn := database.DBUrl(dbConfig)
-	var err error
-	// Add parseTime=true to handle time.Time fields correctly.
-	// It ensures the driver parses MySQL DATETIME/TIMESTAMP columns into time.Time structs.
-	database.DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
-		SkipDefaultTransaction: true, // Improves performance by avoiding auto-transactions.
-		PrepareStmt:            true, // Caches compiled statements for performance and helps prevent SQL injection.
-	})
+	err := database.Database()
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
